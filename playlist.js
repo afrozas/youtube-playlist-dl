f5ed346ff272bca2cf7402454d61437b4ccedee7
enigmaeth <enigmaeth@gmail.com>
2016-11-08 22:49:01 +0530

    Update playlist.js

--- playlist.js
+++ playlist.js
@@ -6,20 +6,31 @@ function playlist(url) {
 
     'use strict';
     var downloaded = 0;
+
+    // arguments passed to youtube-dl
     var video = ytdl(url, ['--format=18'], {
         start: downloaded,
         cwd: pathToFolder
     });
+    
+    // log errors, if anyus
     video.on('error', function error(err) {
         console.log(err.stack);
     });
+
     var size = 0;
+    //called when download starts 
     video.on('info', function(info) {
         downloaded = 0;
         size = info.size;
+
+        //output file name
         var output = path.join(pathToFolder + '/', info._filename + '.mp4');
+
         console.log("\n" + info._filename);
         if (fs.existsSync(output)) {
+
+            // if video is partially downloaded
             downloaded = fs.statSync(output).size;
             var total = info.size + downloaded;
             if (downloaded <= info.size) {
@@ -27,19 +38,32 @@ function playlist(url) {
             } else {
                video.on('next', playlist);
             }
+            
+            //append to the existing stream
             video.pipe(fs.createWriteStream(output, {
                 flags: 'a'
             }));
+            
+            //called if video has been already downloaded 
             video.on('complete', function complete(info) {
                 console.log('filename: ' + info._filename + ' already downloaded.');
             });
+            
+            //called on completion of download
             video.on('end', function end() {
                 console.log('finished downloading!');
             });
+            
         } else {
+
+            // starting video download afresh
             console.log('Downloading...');
+            
+            //create a new stream
             video.pipe(fs.createWriteStream(output));
             var pos = 0;
+
+            //show % completion of download
             video.on('data', function data(chunk) {
                 pos += chunk.length;
                 if (size) {
@@ -51,12 +75,15 @@ function playlist(url) {
             });
         }
     });
+
+    //fetch the next video in the playlist
     video.on('next', playlist);
 }
 
 
 
-var url = process.argv[2];
-var pathToFolder = process.argv[3];
-console.log("downloading playlist :: " + url);
-playlist(url);
+var url = process.argv[2];                          // url from terminal
+var pathToFolder = process.argv[3];                 // download folder path from terminal
+console.log("'\n' downloading playlist :: " + url);  
+
+playlist(url);                                      // pass url to function playlist()
